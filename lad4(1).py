cb32209c1e72210b4edd9a5ca6cb43a5e055aa6b
aleksa763 <morina.aleksa@yandex.ru>
2025-10-03 10:58:00 +0400

    Delete lad4(1).py

--- lad4(1).py
+++
@@ -1,5 +0,0 @@
-a = int(input("Введите время в секундах:"))
-b = int(input("Введите время в минутах:" ))
-b_seconds = b*60
-max_time = a if a>b_seconds else b_seconds
-print("Наибольшее время в секундах:", max_time)
